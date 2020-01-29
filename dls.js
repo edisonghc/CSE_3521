b06c9b8452546b27d28a414e677e362186e37093
Edison Gu <hcgu_edison@outlook.com>
2020-01-28 20:03:02 -0500

    commit

--- dls.js
+++ dls.js
@@ -23,4 +23,96 @@ function depth_limited_search(initial_state,depth_limit) {
     In the recursive case, you don't need the above. Building the solution path is a little
     trickier, but I suggest you look into the Array.unshift() function.
   */
+
+  let open = []; //See push()/pop() and unshift()/shift() to operate like stack or queue
+  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+  let closed = new Set(); //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+
+  /***Your code for breadth-first search here***/
+
+  let current = {
+    state: [],
+    predecessor: [],
+    action: [], //will be defined later
+  };
+
+  let found = false;
+
+  //add initial, add adj nodes to queue, pop current from queue into closed
+
+  open.push({
+    state: initial_state,
+    predecessor: [],
+    action: [] //will be defined later
+  });
+
+  while (!found && open.length != 0) {
+    current = open.shift();
+
+    if (!closed.has(state_to_uniqueid(current.state))) {
+
+      if (is_goal_state(current.state)) {
+        found = true;
+      }
+
+      else {
+        let successors = find_successors(current.state);
+        while (successors.length != 0) {
+
+          element = successors.shift();
+
+          let predecessor_path = current.predecessor.map(x => x);
+          predecessor_path.push(current.state);
+
+          let action_path = current.action.map(x => x);
+          action_path.push(element.actionID);
+
+          open.push({
+            state: element.resultState,
+            predecessor: predecessor_path,
+            action: action_path
+          })
+        }
+        closed.add(state_to_uniqueid(current.state));
+
+      }
+    }
+  }
+
+  /*
+  Hint: In order to generate the solution path, you will need to augment
+  the states to store the predecessor/parent state they were generated from
+  and the action that generates the child state from the predecessor state.
+  
+  For example, make a wrapper object that stores the state, predecessor and action.
+  Javascript objects are easy to make:
+  let object={
+  member_name1 : value1,
+  member_name2 : value2
+  };
+  
+  Hint: Because of the way Javascript Set objects handle Javascript objects, you
+  will need to insert (and check for) a representative value instead of the state
+  object itself. The state_to_uniqueid function has been provided to help you with
+  this. For example
+  let state=...;
+  closed.add(state_to_uniqueid(state)); //Add state to closed set
+  if(closed.has(state_to_uniqueid(state))) { ... } //Check if state is in closed set
+  */
+
+  /***Your code to generate solution path here***/
+
+  if (found) {
+    current.predecessor.shift();
+    current.predecessor.push(current.state)
+    return {
+      actions: current.action /*array of action ids*/,
+      states: current.predecessor /*array of states*/
+    };
+  }
+
+  //OR
+
+  //No solution found
+  return null;
 }
